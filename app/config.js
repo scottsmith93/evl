dca48b8edb5c0fb5f39e944134d27c820e278a57
Scott Smith <scttsmth93@gmail.com>
2020-03-26 09:31:05 +0000

    created a v2 of apply for a EVL certificate - still WIP

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Register for an Emergency Volunteer Leave certificate',
 
   // Default port that prototype runs on
   port: '3000',
