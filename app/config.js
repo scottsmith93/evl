15906044fffc5ab3de5f4c932bee531515c8f93e
Scott Smith <scttsmth93@gmail.com>
2020-03-30 12:19:35 +0100

    placing email content into full e2e journey

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply for Emergency Volunteering Leave',
+  serviceName: 'Apply for the Emergency Volunteering Scheme',
 
   // Default port that prototype runs on
   port: '3000',
